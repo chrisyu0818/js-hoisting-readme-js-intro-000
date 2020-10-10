874968a27e59a4bd25411697f93a58480e6104e7
Zhenhua yu <zyu0628@gmail.com>
2020-10-10 17:37:12 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -11,16 +11,18 @@ function crazy() {
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+  thisIsCrazy();
 }
-thisIsCrazy();
+
 
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
 
-  sayMy();
+
   function sayMy() {
-    console.log(name);
     var name = "Kristin";
+    console.log(name);
   }
+  sayMy();
 }
